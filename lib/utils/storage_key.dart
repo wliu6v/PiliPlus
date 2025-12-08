a7c838efe9d7da27c0945e99983d65cab310907e
6v <linvan6v@gmail.com>
2025-12-08 19:21:04 +0800

    feat: enable direct add video to watch later with long press
    Add a new setting option to directly add videos to watch later list when long pressing on video cards, instead of showing the image save dialog. When enabled, long pressing a video card will immediately add it to watch later and show a toast message.

--- lib/utils/storage_key.dart
+++ lib/utils/storage_key.dart
@@ -44,6 +44,8 @@ abstract final class SettingBoxKey {
       fullScreenGestureReverse = 'fullScreenGestureReverse',
       enableBackgroundPlay = 'enableBackgroundPlay',
       continuePlayInBackground = 'continuePlayInBackground',
+      // 长按直接添加到稍后再看
+      longPressToWatchLater = 'longPressToWatchLater',
       appRcmd = 'appRcmd',
       enableSaveLastData = 'enableSaveLastData',
       minDurationForRcmd = 'minDurationForRcmd',
