90046dc63d4adcd1bd7ac82fe5a05b8b51223af7
6v <linvan6v@gmail.com>
2025-12-09 11:23:36 +0800

    feat: refactor watch later page - remove tabs, add swipe-to-delete with undo

--- lib/pages/later/base_controller.dart
+++ lib/pages/later/base_controller.dart
@@ -1,8 +1,10 @@
 import 'package:PiliPlus/models/common/later_view_type.dart';
+import 'package:PiliPlus/models_new/later/list.dart';
 import 'package:PiliPlus/utils/storage.dart';
 import 'package:PiliPlus/utils/storage_key.dart';
 import 'package:PiliPlus/utils/storage_pref.dart';
 import 'package:get/get.dart';
+import 'dart:async';
 
 class LaterBaseController extends GetxController {
   RxBool enableMultiSelect = false.obs;
@@ -16,9 +18,52 @@ class LaterBaseController extends GetxController {
   // 当前激活的 tab 索引
   RxInt currentTabIndex = 0.obs;
 
+  // 撤销相关
+  RxBool showUndo = false.obs;
+  LaterItemModel? deletedItem;
+  int? deletedIndex;
+  Timer? undoTimer;
+  bool isDeleteRequestCompleted = false; // 删除请求是否已完成
+
   void setIsPlayAll(bool isPlayAll) {
     if (this.isPlayAll.value == isPlayAll) return;
     this.isPlayAll.value = isPlayAll;
     GStorage.setting.put(SettingBoxKey.enablePlayAll, isPlayAll);
+  }
+
+  void showUndoButton(LaterItemModel item, int index) {
+    // 取消之前的定时器
+    undoTimer?.cancel();
+    
+    deletedItem = item;
+    deletedIndex = index;
+    isDeleteRequestCompleted = false;
+    showUndo.value = true;
+    
+    // 5秒后自动隐藏
+    undoTimer = Timer(const Duration(seconds: 5), () {
+      showUndo.value = false;
+      deletedItem = null;
+      deletedIndex = null;
+      isDeleteRequestCompleted = false;
+    });
+  }
+
+  void hideUndoButton() {
+    undoTimer?.cancel();
+    showUndo.value = false;
+    deletedItem = null;
+    deletedIndex = null;
+    isDeleteRequestCompleted = false;
+  }
+
+  void markDeleteRequestCompleted() {
+    isDeleteRequestCompleted = true;
+  }
+
+  @override
+  void onClose() {
+    undoTimer?.cancel();
+    super.onClose();
   }
 }
--- lib/pages/later_search/controller.dart
+++ lib/pages/later_search/controller.dart
@@ -4,7 +4,10 @@ import 'package:PiliPlus/models_new/later/data.dart';
 import 'package:PiliPlus/models_new/later/list.dart';
 import 'package:PiliPlus/pages/common/multi_select/base.dart';
 import 'package:PiliPlus/pages/common/search/common_search_controller.dart';
+import 'package:PiliPlus/pages/later/base_controller.dart';
 import 'package:PiliPlus/pages/later/controller.dart' show BaseLaterController;
+import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 
 class LaterSearchController
@@ -16,6 +19,11 @@ class LaterSearchController
   dynamic mid = Get.arguments['mid'];
   dynamic count = Get.arguments['count'];
 
+  late final LaterBaseController _baseCtr = Get.put(LaterBaseController());
+
+  @override
+  LaterBaseController get baseCtr => _baseCtr;
+
   @override
   Future<LoadingState<LaterData>> customGetData() => UserHttp.seeYouLater(
     page: page,
@@ -25,5 +33,25 @@ class LaterSearchController
   @override
   List<LaterItemModel>? getDataList(LaterData response) {
     return response.list;
+  }
+
+  @override
+  Future<void> toViewDel(
+    BuildContext context,
+    int index,
+    int? aid,
+  ) async {
+    // 搜索页面不需要撤销功能，直接删除
+    if (loadingState.value.data == null || index >= loadingState.value.data!.length) {
+      return;
+    }
+    
+    final res = await UserHttp.toViewDel(aids: aid.toString());
+    if (res['status']) {
+      loadingState.value.data!.removeAt(index);
+      loadingState.refresh();
+      updateCount?.call(1);
+    }
+    SmartDialog.showToast(res['msg']);
   }
 }
